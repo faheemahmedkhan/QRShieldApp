a98856277fee0786111f8ee62bf65c9c481a1c73
faheemahmedkhan <faheemahmedkhan32@gmail.com>
2026-05-17 01:26:56 +0500

    commit

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,5 @@
 const { getDefaultConfig } = require('expo/metro-config');
-const path = require('path');
 
 const config = getDefaultConfig(__dirname);
-
-// Resolve @/ alias to project root
-config.resolver.extraNodeModules = {
-  '@': path.resolve(__dirname),
-};
 
 module.exports = config;
